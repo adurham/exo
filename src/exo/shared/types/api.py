5fced785ca77cd9d53b23a71d266ec058936c283
Adam Durham <adam.durham@gmail.com>
2026-02-13 14:42:12 -0600

    Fix: Remove faulty validator in PlaceInstanceParams that rejected enum strings

--- src/exo/shared/types/api.py
+++ src/exo/shared/types/api.py
@@ -228,12 +228,6 @@ class PlaceInstanceParams(BaseModel):
     instance_meta: InstanceMeta = InstanceMeta.MlxRing
     min_nodes: int = 1
 
-    @field_validator("sharding", "instance_meta", mode="plain")
-    @classmethod
-    def use_default(cls, v: object):
-        if not v or not isinstance(v, (Sharding, InstanceMeta)):
-            raise PydanticUseDefault()
-        return v
 
 
 class CreateInstanceParams(BaseModel):
